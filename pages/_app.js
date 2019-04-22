e9c101c12605d29c7552be724ced38968b17a318
Javier Prado <jprado@f-m.fm>
2019-04-22 13:45:01 -0500

    Added the redux-saga middleware to the next.js app.

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,23 @@
 import React from 'react';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import App, { Container } from 'next/app';
 import withRedux from 'next-redux-wrapper';
+import createSagaMiddleware from 'redux-saga';
 
+import rootSaga from '../src/features/rejection/rejection-sagas';
 import rejection from '../src/features/rejection/rejection-reducer';
 
 const makeStore = (initialState = [], options) => {
-  return createStore(rejection, initialState);
+  const saga = createSagaMiddleware();
+  const store = createStore(rejection, initialState, applyMiddleware(saga));
+  saga.run(rootSaga);
+
+  return store;
 };
 
 class MyApp extends App {
   static async getInitialProps({Component, ctx}) {
-    ctx.store.dispatch({type: 'FOO', payload: 'foo'});
-
     const pageProps = Component.getInitialProps ? await Component.getInitialProps(ctx) : {};
 
     return {pageProps};
@@ -21,6 +25,7 @@ class MyApp extends App {
 
   render() {
     const {Component, pageProps, store} = this.props;
+
     return (
       <Container>
         <Provider store={store}>
